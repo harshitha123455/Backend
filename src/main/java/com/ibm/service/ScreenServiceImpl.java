8db0f5deb3bd01584e64b64a034eb5e84f9c9be9
JestinSabu <jestinsabu721@gmail.com>
2023-06-06 20:11:11 +0530

    Removed unncessary imports

--- src/main/java/com/ibm/service/ScreenServiceImpl.java
+++ src/main/java/com/ibm/service/ScreenServiceImpl.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.ibm.entity.Movie;
 import com.ibm.entity.Screen;
 import com.ibm.repo.ScreenRepository;
 
