d7b2428806a200c00a96686b2ab492e6e961a472
JestinSabu <jestinsabu721@gmail.com>
2023-06-09 10:12:10 +0530

    Add comments

--- src/main/java/com/ibm/rest/AdminController.java
+++ src/main/java/com/ibm/rest/AdminController.java
@@ -29,45 +29,52 @@ public class AdminController {
 	@Autowired
 	private AdminService service;
 
+//	http://localhost:8880/admin/add
 	@PostMapping(path = "/add", consumes = "application/json")
 	public ResponseEntity<String> saveAdmin(@RequestBody AdminDetails adminDetails) {
 		int id = service.save(adminDetails);
-		return ResponseEntity.status(HttpStatus.ACCEPTED).header("Response from", "MovieController")
+		return ResponseEntity.status(HttpStatus.ACCEPTED).header("Response from", "AdminController")
 				.body("Admin added with id: " + id);
 	}
 
+//	http://localhost:8880/admin/all
 	@GetMapping(path = "/all", produces = "application/json")
 	public ResponseEntity<List<Admin>> getAllAdmins() {
-		return ResponseEntity.status(HttpStatus.ACCEPTED).header("Response from", "MovieController")
+		return ResponseEntity.status(HttpStatus.ACCEPTED).header("Response from", "AdminController")
 				.body(service.list());
 	}
 
+//	http://localhost:8880/admin/id/{id}
 	@GetMapping(path = "/id/{id}", produces = "application/json")
 	public ResponseEntity<Admin> getAdminById(@PathVariable int id) {
-		return ResponseEntity.status(HttpStatus.ACCEPTED).header("Response from", "MovieController")
+		return ResponseEntity.status(HttpStatus.ACCEPTED).header("Response from", "AdminController")
 				.body(service.searchById(id));
 	}
 
+//	http://localhost:8880/admin/email/{email}
 	@GetMapping(path = "/email/{email}", produces = "application/json")
 	public ResponseEntity<Admin> getAdminByEmail(@PathVariable String email) {
-		return ResponseEntity.status(HttpStatus.ACCEPTED).header("Response from", "MovieController")
+		return ResponseEntity.status(HttpStatus.ACCEPTED).header("Response from", "AdminController")
 				.body(service.searchByEmail(email));
 	}
 
-	@DeleteMapping(path = "id/{id}", produces = "application/json")
+//	http://localhost:8880/admin/remove/id/{id}
+	@DeleteMapping(path = "/remove/id/{id}", produces = "application/json")
 	public ResponseEntity<String> deleteAdmin(@PathVariable int id) {
-		return ResponseEntity.status(HttpStatus.ACCEPTED).header("Response from", "MovieController")
+		return ResponseEntity.status(HttpStatus.ACCEPTED).header("Response from", "AdminController")
 				.body("Admin with id: " + id + " deleted successfully");
 	}
 
+//	http://localhost:8880/admin/login
 	@PostMapping(path = "/login", consumes = "application/json")
 	public ResponseEntity<Map<String, String>> authenticateAdmin(@RequestBody AdminLogin a) {
 		String token = service.authenticate(a.getEmail(), a.getPassword());
-		if(token == null)
+		if (token == null)
 			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
 		Map<String, String> response = new HashMap<>();
 		response.put("token", token);
-		return ResponseEntity.status(HttpStatus.ACCEPTED).body(response);
+		return ResponseEntity.status(HttpStatus.ACCEPTED).header("Response from", "AdminController")
+				.body(response);
 	}
 
 }
