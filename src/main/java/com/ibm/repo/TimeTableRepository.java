9637de08c7618e732a819cd7ba724a3987adfa46
JestinSabu <jestinsabu721@gmail.com>
2023-06-15 21:37:26 +0530

    Added show search in timetable

--- src/main/java/com/ibm/repo/TimeTableRepository.java
+++ src/main/java/com/ibm/repo/TimeTableRepository.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ibm.entity.Screen;
+import com.ibm.entity.Shows;
 import com.ibm.entity.TimeTable;
 
 public interface TimeTableRepository extends JpaRepository<TimeTable, Integer> {
@@ -15,5 +16,7 @@ public interface TimeTableRepository extends JpaRepository<TimeTable, Integer> {
 	List<TimeTable> findAllByDate(Date date);
 	
 	TimeTable findByDateAndScreen(Date date, Screen screen);
+	
+	TimeTable findBySlot1OrSlot2OrSlot3OrSlot4(Shows s1, Shows s2, Shows s3, Shows s4);
 
 }
